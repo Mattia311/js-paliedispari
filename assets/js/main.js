db1bc6b12d54a925d3a4e918b6f0fe0f3b89488b
Mattia311 <mattia.space98@gmail.com>
2021-10-19 15:56:43 +0200

    somma dei due risultati

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 //Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
 
-const askWord = prompt('Inserisci una parola');
+/*const askWord = prompt('Inserisci una parola');
 const text = document.getElementById('text');
 
 function reverse(s){
@@ -28,4 +28,32 @@ function wordPoli (message) {
     console.log('questa parola non è polindroma: ' + askWord);
     text.innerHTML = ('questa parola non è polindroma: ' + askWord)
   
+  }*/
+
+
+
+
+
+//L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.
+
+
+const askPariDispari = prompt('Scegli tra pari e dispari');
+const typeNumber = parseInt(prompt('Scegli un numero da 1 a 5'));
+
+console.log('Hai scelto ' + askPariDispari, 'e il numero ' + typeNumber);
+
+const min = 1
+const max = 5
+
+function getRandomIntInclusive(min, max) {
+    
+    return Math.floor(Math.random() * (5 - 1 + 1) + 1); 
   }
+
+  console.log('Il numero random generato per il computer è : ' + getRandomIntInclusive(min, max));
+
+  const somma = typeNumber + getRandomIntInclusive(min, max);
+  console.log(somma);
+
+  
+
