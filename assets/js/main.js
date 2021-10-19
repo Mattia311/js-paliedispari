f18a533f6d3643d528da62df530dd544429764a1
Mattia311 <mattia.space98@gmail.com>
2021-10-19 17:01:19 +0200

    esercizio completo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 //Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
 
-/*const askWord = prompt('Inserisci una parola');
+const askWord = prompt('Inserisci una parola');
 const text = document.getElementById('text');
 
 function reverse(s){
@@ -28,7 +28,7 @@ function wordPoli (message) {
     console.log('questa parola non è polindroma: ' + askWord);
     text.innerHTML = ('questa parola non è polindroma: ' + askWord)
   
-  }*/
+  }
 
 
 
@@ -48,12 +48,48 @@ const max = 5
 function getRandomIntInclusive(min, max) {
     
     return Math.floor(Math.random() * (5 - 1 + 1) + 1); 
+}
+
+  var result = getRandomIntInclusive(min, max)
+
+  console.log('Il numero random generato per il computer è : ' + result);
+
+  const somma = typeNumber + result;
+  console.log('questa è la somma tra il tuo numero e quello del computer: ' + somma);
+
+
+  function pariDispari () {
+    if(somma % 2 == 0) {
+      return 'pari'
+    } 
+    else {
+      return 'dispari'
+    }
+  };
+
+  pariDispari(somma);
+  
+  if (pariDispari(somma) == 'pari') {
+    console.log('questo numero è pari' , somma);
   }
+  else {
+    console.log('questo numero è dispari' , somma);
+  };
 
-  console.log('Il numero random generato per il computer è : ' + getRandomIntInclusive(min, max));
+  if((askPariDispari == 'pari') && (pariDispari(somma) == 'pari')) {
+      console.log('Il numero uscito dalla somma del tuo numero e quello generato in modo random dal computer è pari, hai vinto: ' + somma);
 
-  const somma = typeNumber + getRandomIntInclusive(min, max);
-  console.log(somma);
+  } 
+  else if((askPariDispari == 'pari') && (pariDispari(somma) == 'dispari')) {
+      console.log('Il numero uscito dalla somma del tuo numero e quello generato in modo random dal computer è dispari, hai perso: ' + somma);
+  }
+   else if((askPariDispari == 'dispari') && (pariDispari(somma) == 'dispari')) {
+      console.log('Il numero uscito dalla somma del tuo numero e quello generato in modo random dal computer è dispari, hai vinto: ' + somma);
+   } 
+   else if((askPariDispari == 'dispari') && (pariDispari(somma) == 'pari')) {
+      console.log('Il numero uscito dalla somma del tuo numero e quello generato in modo random dal computer è pari, hai perso: ' + somma);
+   };
+  
 
   
 
